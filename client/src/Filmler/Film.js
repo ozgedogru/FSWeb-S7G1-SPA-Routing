f63a147f6c7d0db80f009897f30d1b66768fc09e
ozgedogru <ozgenurkarademir@gmail.com>
2023-10-30 23:45:27 +0300

    done

--- client/src/Filmler/Film.js
+++ client/src/Filmler/Film.js
@@ -1,25 +1,28 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
 export default function Film(props) {
   const [movie, setMovie] = useState();
 
-  let id = 1;
+  const { id } = useParams();
   // URL'den alınan :id parametresini bu değişkene aktarın
 
   useEffect(() => {
     axios
       .get(`http://localhost:5001/api/filmler/${id}`) // Bu uç noktayı Postman'le çalışın
-      .then(response => {
-          // Bu kısmı log statementlarıyla çalışın
-          // ve burdan gelen response'u 'movie' e aktarın
+      .then((response) => {
+        console.log(response.data);
+        setMovie(response.data);
+        // Bu kısmı log statementlarıyla çalışın
+        // ve burdan gelen response'u 'movie' e aktarın
       })
-      .catch(error => {
+      .catch((error) => {
         console.error(error);
       });
     // Bu effect her `id ` değiştiğinde çalışmalı
     // Bunu nasıl gerçekleştirebiliriz?
-  }, []);
+  }, [id]);
 
   // Yalnızca esnek görevlere geçtiğinizde burdaki yorum etiketini kaldırın
   // const filmiKaydet = evt => { }
@@ -33,7 +36,7 @@ export default function Film(props) {
   return (
     <div className="save-wrapper">
       <div className="movie-card">
-        <h2>{title}</h2>
+        <h2>Film adi {title}</h2>
         <div className="movie-director">
           Director: <em>{director}</em>
         </div>
@@ -42,7 +45,7 @@ export default function Film(props) {
         </div>
         <h3>Actors</h3>
 
-        {stars.map(star => (
+        {stars.map((star) => (
           <div key={star} className="movie-star">
             {star}
           </div>
--- client/src/Filmler/FilmListesi.js
+++ client/src/Filmler/FilmListesi.js
@@ -1,20 +1,21 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom/";
 
 export default function FilmListesi(props) {
   return (
     <div className="movie-list">
-      {props.movies.map(movie => (
+      {props.movies.map((movie) => (
         <FilmDetayları key={movie.id} movie={movie} />
       ))}
     </div>
   );
-  }
+}
 
 function FilmDetayları(props) {
-  const { title, director, metascore } = props.movie;
+  const { id, title, director, metascore } = props.movie;
 
   return (
-    <div className="movie-card">
+    <Link to={`/${id}`} className="movie-card">
       <h2>{title}</h2>
       <div className="movie-director">
         Director: <em>{director}</em>
@@ -22,6 +23,6 @@ function FilmDetayları(props) {
       <div className="movie-metascore">
         Metascore: <strong>{metascore}</strong>
       </div>
-    </div>
+    </Link>
   );
 }
